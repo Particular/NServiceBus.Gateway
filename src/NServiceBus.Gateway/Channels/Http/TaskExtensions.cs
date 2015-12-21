6c7171033d6814c4e924445200d92520024184c2
Jan Ove Skogheim <janovesk@janovesk.com>
2015-12-21 14:45:31 +0100

    Merged in async changes.
    Closes https://github.com/Particular/NServiceBus.Gateway/pull/15

---
+++ src/NServiceBus.Gateway/Channels/Http/TaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NServiceBus.Gateway.Channels.Http
+{
+    using System.Threading.Tasks;
+
+    static class TaskExtensions
+    {
+        // ReSharper disable once UnusedParameter.Global
+        public static void Forget(this Task task)
+        {
+        }
+    }
+}
