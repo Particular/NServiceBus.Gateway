b63114c9248e550f3ed584a8218601ddf7655cfe
Simon Cropp <simon.cropp@gmail.com>
2016-04-07 20:32:32 +1000

    update references

---
+++ src/NServiceBus.Gateway/ElevateChecker.cs
@@ -0,0 +1,16 @@
+namespace NServiceBus.Installation
+{
+    using System.Security.Principal;
+
+    static class ElevateChecker
+    {
+        public static bool IsCurrentUserElevated()
+        {
+            using (var windowsIdentity = WindowsIdentity.GetCurrent())
+            {
+                var windowsPrincipal = new WindowsPrincipal(windowsIdentity);
+                return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+    }
+}
