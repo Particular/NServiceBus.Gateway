5b1de5fbcb156c1fec43c2253878190a7c4cd126
Simon Cropp <simon.cropp@gmail.com>
2016-04-16 09:30:35 +1000

    fix copyright

--- src/NServiceBus.Gateway/AssemblyInfo.cs
+++ src/NServiceBus.Gateway/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NServiceBus.Gateway")]
-[assembly: AssemblyCopyright("Copyright 2010-2014 NServiceBus. All rights reserved")]
+[assembly: AssemblyCopyright("Copyright NServiceBus. All rights reserved")]
 [assembly: AssemblyProduct("NServiceBus.Gateway")]
 [assembly: AssemblyCompany("NServiceBus Ltd.")]
 [assembly: ComVisible(false)]
