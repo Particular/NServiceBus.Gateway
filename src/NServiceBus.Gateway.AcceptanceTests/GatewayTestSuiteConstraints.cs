629411811b77be5e717c1283a97e8fc953fbc086
andreasohlund <andreas.ohlund@particular.net>
2017-12-18 13:04:39 +0100

    Missing file

--- src/NServiceBus.Gateway.AcceptanceTests/GatewayTestSuiteConstraints.cs
+++ src/NServiceBus.Gateway.AcceptanceTests/GatewayTestSuiteConstraints.cs
@@ -1,22 +1,7 @@
 ﻿namespace NServiceBus.Gateway.AcceptanceTests
 {
-    using System.Threading.Tasks;
-    using AcceptanceTesting.Support;
-
     public partial class GatewayTestSuiteConstraints : IGatewayTestSuiteConstraints
     {
         public static GatewayTestSuiteConstraints Current = new GatewayTestSuiteConstraints();
-    }
-
-    public interface IGatewayTestSuiteConstraints
-    {
-        IConfigureGatewayPersitenceExecution CreatePersistenceConfiguration();
-    }
-
-    public interface IConfigureGatewayPersitenceExecution
-    {
-        Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings);
-
-        Task Cleanup();
     }
 }
