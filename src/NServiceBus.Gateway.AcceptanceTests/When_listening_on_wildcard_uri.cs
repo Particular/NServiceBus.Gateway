ba31945e8921b73ef9a24471e629b102567631a3
Andreas Öhlund <andreas.ohlund@particular.net>
2018-10-03 08:37:45 +0200

    Fix site and channel api bug (#100)
    * Fix bug with listening on multiple channels.
    
    * Add test for gateway listening on multiple URIs
    
    * throw an exception with address details when an invalid address is used
    
    * Get the hostname for the local computer in a way that should run the same on windows & linux.
    
    * Use different URIs for wildcard tests
    
    * Make sure that both test endpoints have started
    
    * Avoid port clash
    
    * Capitalise start of test names.
    
    * Changing ports in second wildcard test
    
    * listen on wildcard first, then specific hostname.
    
    * use unique port
    
    * No need for started check
    
    * Move test message inside tests
    
    * Consolidate test
    
    * Fix csproj
    
    * Revert port change
    
    * Simplify logic
    
    * cleanup code formatting
    
    * fix multi-site issue
    
    * Make port unique
    
    * Remove user agent

--- src/NServiceBus.Gateway.AcceptanceTests/When_listening_on_wildcard_uri.cs
+++ src/NServiceBus.Gateway.AcceptanceTests/When_listening_on_wildcard_uri.cs
@@ -7,7 +7,7 @@
     public class When_listening_on_wildcard_uri : NServiceBusAcceptanceTest
     {
         [Test]
-        public void should_throw_exception_if_wildcard_channel_is_used_for_replies()
+        public void Should_throw_exception_if_wildcard_channel_is_used_for_replies()
         {
             Assert.That(async () =>
             {
@@ -25,7 +25,30 @@
                 {
                     var gatewaySettings = c.Gateway();
 
-                    gatewaySettings.AddReceiveChannel("http://+:25699/");
+                    gatewaySettings.AddReceiveChannel("http://+:25601/WildcardA/");
+                });
+            }
+        }
+
+        [Test]
+        public void Should_not_throw_exception_if_wildcard_channel_is_not_default()
+        {
+            Assert.DoesNotThrowAsync(async () =>
+                await Scenario.Define<ScenarioContext>()
+                    .WithEndpoint<EndpointWithWildCardUriAndFullyQualifiedDefault>(e => e.When(b => Task.FromResult(0)))
+                    .Run());
+        }
+
+        class EndpointWithWildCardUriAndFullyQualifiedDefault : EndpointConfigurationBuilder
+        {
+            public EndpointWithWildCardUriAndFullyQualifiedDefault()
+            {
+                EndpointSetup<GatewayEndpoint>(c =>
+                {
+                    var gatewaySettings = c.Gateway();
+
+                    gatewaySettings.AddReceiveChannel("http://+:25701/WildcardB/");
+                    gatewaySettings.AddReceiveChannel("http://localhost:25700/WildcardB/", isDefault: true);
                 });
             }
         }
