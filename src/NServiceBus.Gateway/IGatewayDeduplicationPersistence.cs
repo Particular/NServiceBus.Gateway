add4f9b73e829ebb9a65bfb59b2f8504428eede7
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-11-27 16:31:59 +0100

    define new gateway persistence API

---
+++ src/NServiceBus.Gateway/IGatewayDeduplicationPersistence.cs
@@ -0,0 +1,30 @@
+﻿namespace NServiceBus
+{
+    using System.Threading.Tasks;
+    using System.Transactions;
+    using Extensibility;
+
+    /// <summary>
+    /// Defines the api to provide storage for the gateway deduplication mechanism.
+    /// </summary>
+    public interface IGatewayDeduplicationStorage
+    {
+        /// <summary>
+        /// Returns whether the storage can enlist in a distributed transaction via ambient <see cref="TransactionScope"/> to support exactly-once dispatching.
+        /// </summary>
+        bool SupportsDistributedTransactions { get; }
+
+        /// <summary>
+        /// Returns if the message is a duplicate.
+        /// </summary>
+        /// <returns>
+        /// <code>true</code> if the message has been receied successfully before and is considered a duplicate. <code>false</code> otherwise.
+        /// </returns>
+        Task<bool> IsDuplicate(string messageId, ContextBag context);
+
+        /// <summary>
+        /// Marks the message as successfully dispatched. Marking a message as dispatched will consider it a duplicate when invoking <see cref="IsDuplicate"/>.
+        /// </summary>
+        Task MarkAsDispatched(string messageId, ContextBag context);
+    }
+}
