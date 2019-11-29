64a860047c6c253d6bc7d1d76abaedf1c1532482
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-11-29 11:38:03 +0100

    use new seam API internally

---
+++ src/NServiceBus.Gateway/Deduplication/LegacyDeduplicationWrapper.cs
@@ -0,0 +1,30 @@
+﻿namespace NServiceBus.Gateway.Deduplication
+{
+    using System;
+    using System.Threading.Tasks;
+    using Extensibility;
+
+    class LegacyDeduplicationWrapper : IGatewayDeduplicationStorage
+    {
+        IDeduplicateMessages legacyPersister;
+
+        public LegacyDeduplicationWrapper(IDeduplicateMessages legacyPersister)
+        {
+            this.legacyPersister = legacyPersister;
+        }
+
+        public bool SupportsDistributedTransactions { get; } = true;
+
+        public async Task<bool> IsDuplicate(string messageId, ContextBag context)
+        {
+            var isNewMessage = await legacyPersister.DeduplicateMessage(messageId, DateTime.UtcNow, context)
+                .ConfigureAwait(false);
+            return !isNewMessage;
+        }
+
+        public Task MarkAsDispatched(string messageId, ContextBag context)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
