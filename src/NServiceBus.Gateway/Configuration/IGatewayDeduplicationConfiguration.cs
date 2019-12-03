f19539f8e80c8d2b7654bf54531160d264684570
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-12-03 16:49:43 +0100

    add configuration API

---
+++ src/NServiceBus.Gateway/Configuration/IGatewayDeduplicationConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace NServiceBus.Gateway
+{
+    using ObjectBuilder;
+
+    /// <summary>
+    /// Configures the deduplication storage.
+    /// </summary>
+    public interface IGatewayDeduplicationConfiguration
+    {
+        /// <summary>
+        /// Creates an instance of the deduplication storage.
+        /// </summary>
+        /// <param name="builder"></param>
+        IGatewayDeduplicationStorage CreateStorage(IBuilder builder);
+    }
+}
