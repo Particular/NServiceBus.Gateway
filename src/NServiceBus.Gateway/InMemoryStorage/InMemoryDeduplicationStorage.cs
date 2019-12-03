b4e44267c9fffb5257e36fdcafdbf7441c0eb4fb
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-12-03 13:46:15 +0100

    Add core based inmemory deduplication implementation

--- src/NServiceBus.Gateway/InMemoryStorage/InMemoryDeduplicationStorage.cs
+++ src/NServiceBus.Gateway/InMemoryStorage/InMemoryDeduplicationStorage.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NServiceBus.Gateway.InMemoryStorage
+﻿namespace NServiceBus.Gateway
 {
-    class InMemoryDeduplicationStorage
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Extensibility;
+
+    class InMemoryDeduplicationStorage : IGatewayDeduplicationStorage
     {
-    }
+        readonly int cacheSize;
+        readonly LinkedList<string> clientIdList = new LinkedList<string>();
+        readonly Dictionary<string, LinkedListNode<string>> clientIdSet = new Dictionary<string, LinkedListNode<string>>();
+
+        public InMemoryDeduplicationStorage(int cacheSize = 10000)
+        {
+            this.cacheSize = cacheSize;
+        }
+
+        public bool SupportsDistributedTransactions { get; } = false;
+
+        public Task<bool> IsDuplicate(string messageId, ContextBag context)
+        {
+            lock (clientIdSet)
+            {
+                return Task.FromResult(clientIdSet.ContainsKey(messageId));
+            }
+        }
+
+        public Task MarkAsDispatched(string messageId, ContextBag context)
+        {
+            lock (clientIdSet)
+            {
+                if (clientIdSet.TryGetValue(messageId, out var existingNode)) // O(1)
+                {
+                    // "refresh" lifetime by moving id to the back of the linked list
+                    clientIdList.Remove(existingNode); // O(1) operation, because we got the node reference
+                    clientIdList.AddLast(existingNode); // O(1) operation
+                }
+                else
+                {
+                    if (clientIdSet.Count == cacheSize)
+                    {
+                        var id = clientIdList.First.Value;
+                        clientIdSet.Remove(id); // O(1)
+                        clientIdList.RemoveFirst(); // O(1)
+                    }
+
+                    var node = clientIdList.AddLast(messageId); // O(1)
+                    clientIdSet.Add(messageId, node); // O(1)
+                }
+
+                return Task.FromResult(0);
+            }
+        }
+    }   
 }
