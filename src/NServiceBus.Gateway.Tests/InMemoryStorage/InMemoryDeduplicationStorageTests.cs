3edafcc53f41ea9b46027e6f98a8f0d470eb6fcb
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-12-04 16:51:27 +0100

    In-memory implementation of the new Gateway API (#136)
    * initial implementation
    
    * Add core based inmemory deduplication implementation
    
    * add test for LRU cache behavior
    
    * add InMemoryDeduplicationConfiguration class
    
    * approve API

---
+++ src/NServiceBus.Gateway.Tests/InMemoryStorage/InMemoryDeduplicationStorageTests.cs
@@ -0,0 +1,27 @@
+﻿namespace NServiceBus.Gateway.Tests.InMemoryStorage
+{
+    using System.Threading.Tasks;
+    using Extensibility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class InMemoryDeduplicationStorageTests
+    {
+        [Test]
+        public async Task Should_remove_oldest_entries_when_LRU_reaches_limit()
+        {
+            var storage = new InMemoryDeduplicationStorage(2);
+
+            await storage.MarkAsDispatched("A", new ContextBag());
+            Assert.True(await storage.IsDuplicate("A", new ContextBag()));
+
+            await storage.MarkAsDispatched("B", new ContextBag());
+            Assert.True(await storage.IsDuplicate("A", new ContextBag()));
+
+            await storage.MarkAsDispatched("C", new ContextBag());
+            Assert.True(await storage.IsDuplicate("B", new ContextBag()));
+            Assert.True(await storage.IsDuplicate("C", new ContextBag()));
+            Assert.False(await storage.IsDuplicate("A", new ContextBag()));
+        }
+    }
+}
