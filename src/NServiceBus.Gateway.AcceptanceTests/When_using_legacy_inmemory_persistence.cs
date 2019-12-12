769c437e10d2489cffe7ca7a9af81214c0b9733a
Tim Bussmann <timbussmann@users.noreply.github.com>
2019-12-12 13:19:50 +0100

    add test to verify warning when using core's inmemory implementation

---
+++ src/NServiceBus.Gateway.AcceptanceTests/When_using_legacy_inmemory_persistence.cs
@@ -0,0 +1,39 @@
+﻿namespace NServiceBus.Gateway.AcceptanceTests
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using AcceptanceTesting;
+    using Logging;
+    using NUnit.Framework;
+
+    public class When_using_legacy_inmemory_persistence : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public async Task Should_log_warning()
+        {
+            var context = await Scenario.Define<ScenarioContext>()
+                .WithEndpoint<EndpointWithLegacyConfiguration>()
+                .Done(c => c.EndpointsStarted)
+                .Run();
+
+            var warnings = context.Logs
+                .Where(l => l.Level == LogLevel.Warn)
+                .Select(l => l.Message);
+            Assert.That(warnings.Any(l => l.Contains("Endpoint is configured to use the legacy in-memory gateway deduplication storage")));
+        }
+
+        class EndpointWithLegacyConfiguration : EndpointConfigurationBuilder
+        {
+            public EndpointWithLegacyConfiguration()
+            {
+                EndpointSetup<GatewayEndpointWithNoStorage>(c =>
+                {
+                    c.UsePersistence<InMemoryPersistence, StorageType.GatewayDeduplication>();
+                    
+                    // legacy configuration API (not passing a storage as parameter):
+                    c.Gateway();
+                });
+            }
+        }
+    }
+}
