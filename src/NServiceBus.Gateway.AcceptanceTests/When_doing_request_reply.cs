73fc6968e7b8c66d23f6f5c7dac02299e896779f
Alexis Denis Martinez <alexis.denis@external.wolterskluwer.com>
2021-06-17 15:35:46 +0200

    Implement a Proxy address on channel configuration than will used as public address for a reply and for a wildcard verification as talk on https://github.com/Particular/NServiceBus.Gateway/pull/267

--- src/NServiceBus.Gateway.AcceptanceTests/When_doing_request_reply.cs
+++ src/NServiceBus.Gateway.AcceptanceTests/When_doing_request_reply.cs
@@ -27,10 +27,46 @@
             Assert.AreEqual(1, context.Response);
         }
 
+        [Test]
+        public async Task Callback_should_be_fired_when_SideAProxy_IsBehind_Proxy()
+        {
+            var context = await Scenario.Define<Context>()
+                              .WithEndpoint<SiteAWithProxy>(
+                                  b => b.When(async (bus, c) =>
+                                      {
+                                          var options = new SendOptions();
+                                          options.RouteToSites("SiteB");
+                                          c.Response = await bus.Request<int>(new MyRequest(), options);
+                                          c.GotCallback = true;
+                                      }))
+                              .WithEndpoint<SiteB>()
+                              .Done(c => c.GotCallback)
+                              .Run();
+
+            Assert.IsTrue(context.GotCallback);
+            Assert.AreEqual(1, context.Response);
+        }
+
         public class Context : ScenarioContext
         {
             public bool GotCallback { get; set; }
             public int Response { get; set; }
+        }
+
+        public class SiteAWithProxy : EndpointConfigurationBuilder
+        {
+            public SiteAWithProxy()
+            {
+                EndpointSetup<GatewayEndpoint>(c =>
+                    {
+                        c.MakeInstanceUniquelyAddressable("1");
+                        c.EnableCallbacks();
+
+                        var gatewaySettings = c.GetSettings().Get<GatewaySettings>();
+                        gatewaySettings.AddReceiveChannel("http://+:25699/SiteA/", "http", 10, true, "http://localhost:25699/SiteA/");
+                        gatewaySettings.AddSite("SiteB", "http://localhost:25699/SiteB/");
+                    });
+            }
         }
 
         public class SiteA : EndpointConfigurationBuilder
