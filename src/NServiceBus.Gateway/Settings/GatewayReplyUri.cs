79521b72c28240d8fc60a2f78d2b9f70f249acce
David Boike <david.boike@gmail.com>
2021-06-25 13:52:32 -0500

    Add SetReplyToUri()

---
+++ src/NServiceBus.Gateway/Settings/GatewayReplyUri.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus
+{
+    class GatewayReplyUri
+    {
+        public string Type { get; }
+        public string Address { get; }
+
+        internal GatewayReplyUri(string type, string address)
+        {
+            Type = type;
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type},{Address}";
+        }
+    }
+}
