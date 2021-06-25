b22b03cba4beb1bf0b94f516e161e150c17a7bca
David Boike <david.boike@gmail.com>
2021-06-25 13:56:52 -0500

    Adjust exception message

--- src/NServiceBus.Gateway/Gateway.cs
+++ src/NServiceBus.Gateway/Gateway.cs
@@ -121,7 +121,7 @@
 
             if (replyToAddress.address.Contains("*") || replyToAddress.address.Contains("+"))
             {
-                throw new Exception($"The address {replyToAddress.address} is configured as the reply-to address, but contains a wildcard in the URI, which would not be addressable for a reply. Please use `gatewaySettings.SetReplyToAddress(address)` with a non-wildcard address in order for replies to be transmitted properly.");
+                throw new Exception($"The address {replyToUri.Address} is configured as the reply-to URI, but contains a wildcard in the URI, which would not be addressable for a reply. Please use `gatewaySettings.SetReplyToAddress(address)` with a non-wildcard address in order for replies to be transmitted properly.");
             }
 
             if (!channelManager.GetReceiveChannels().Any(channel => channel.Type == replyToAddress.type))
