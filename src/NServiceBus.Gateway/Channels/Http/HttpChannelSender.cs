5e444e6af0ac2b39479e8c393fc97860cb963579
David Boike <david.boike@gmail.com>
2022-05-11 15:02:13 -0500

    net6.0

--- src/NServiceBus.Gateway/Channels/Http/HttpChannelSender.cs
+++ src/NServiceBus.Gateway/Channels/Http/HttpChannelSender.cs
@@ -4,9 +4,10 @@ namespace NServiceBus.Gateway.Channels.Http
     using System.Collections.Generic;
     using System.IO;
     using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
     using System.Threading;
     using System.Threading.Tasks;
-    using System.Web;
     using Logging;
 
     [ChannelType("http")]
@@ -15,37 +16,26 @@ namespace NServiceBus.Gateway.Channels.Http
     {
         public async Task Send(string remoteUrl, IDictionary<string, string> headers, Stream data, CancellationToken cancellationToken = default)
         {
-            var request = WebRequest.Create(remoteUrl);
 
-            request.Method = "POST";
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.Headers = Encode(headers);
-            request.UseDefaultCredentials = true;
-            request.ContentLength = data.Length;
 
-            using (var stream = await request.GetRequestStreamAsync().ConfigureAwait(false))
+            var request = new HttpRequestMessage(HttpMethod.Post, remoteUrl);
+
+            foreach (var pair in headers)
             {
-                // 81920 is the default value in the underlying code.
-                // .NET Framework does not have an overload that accepts only Stream and CancellationToken
-                await data.CopyToAsync(stream, 81920, cancellationToken).ConfigureAwait(false);
+                request.Headers.Add(pair.Key, pair.Value);
             }
+
+            request.Content = new StreamContent(data);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
 
             HttpStatusCode statusCode;
 
-            try
+            //todo make the receiver send the md5 back so that we can double check that the transmission went ok
+            using (var httpClient = new HttpClient())
             {
-                //todo make the receiver send the md5 back so that we can double check that the transmission went ok
-                using (var response = (HttpWebResponse)await request.GetResponseAsync().ConfigureAwait(false))
-                {
-                    statusCode = response.StatusCode;
-                }
+                var response = await httpClient.SendAsync(request, cancellationToken).ConfigureAwait(false);
+                statusCode = response.StatusCode;
             }
-            catch (WebException ex)
-            {
-                ex.Response?.Dispose();
-                throw;
-            }
-
 
             Logger.Debug("Got HTTP response with status code " + statusCode);
 
@@ -55,19 +45,6 @@ namespace NServiceBus.Gateway.Channels.Http
                 throw new Exception("Retrying");
             }
         }
-
-        static WebHeaderCollection Encode(IDictionary<string, string> headers)
-        {
-            var webHeaders = new WebHeaderCollection();
-
-            foreach (var pair in headers)
-            {
-                webHeaders.Add(HttpUtility.UrlEncode(pair.Key), HttpUtility.UrlEncode(pair.Value));
-            }
-
-            return webHeaders;
-        }
-
 
         static ILog Logger = LogManager.GetLogger<HttpChannelSender>();
     }
