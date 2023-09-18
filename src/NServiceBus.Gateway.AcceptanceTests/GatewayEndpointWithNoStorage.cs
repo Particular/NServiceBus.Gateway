25dee7106d1a38be1206b5158f1ce7ecfc0bc883
Szymon Pobiega <szymon.pobiega@gmail.com>
2023-09-18 08:52:30 +0200

    Update to new acceptance testing

--- src/NServiceBus.Gateway.AcceptanceTests/GatewayEndpointWithNoStorage.cs
+++ src/NServiceBus.Gateway.AcceptanceTests/GatewayEndpointWithNoStorage.cs
@@ -1,35 +1,8 @@
 ﻿namespace NServiceBus.Gateway.AcceptanceTests
 {
-    using System;
-    using System.IO;
-    using System.Threading.Tasks;
-    using AcceptanceTesting.Customization;
-    using AcceptanceTesting.Support;
-    using NUnit.Framework;
+    using NServiceBus.AcceptanceTesting.EndpointTemplates;
 
-    public class GatewayEndpointWithNoStorage : IEndpointSetupTemplate
+    public class GatewayEndpointWithNoStorage : ServerWithNoDefaultPersistenceDefinitions
     {
-        public virtual async Task<EndpointConfiguration> GetConfiguration(RunDescriptor runDescriptor, EndpointCustomizationConfiguration endpointCustomizationConfiguration, Func<EndpointConfiguration, Task> configurationBuilderCustomization)
-        {
-            var endpointConfiguration = new EndpointConfiguration(endpointCustomizationConfiguration.EndpointName);
-
-            endpointConfiguration.TypesToIncludeInScan(endpointCustomizationConfiguration.GetTypesScopedByTestClass());
-
-            endpointConfiguration.Recoverability()
-                .Delayed(delayed => delayed.NumberOfRetries(0))
-                .Immediate(immediate => immediate.NumberOfRetries(0));
-
-            var storageDir = Path.Combine(NServiceBusAcceptanceTest.StorageRootDir, TestContext.CurrentContext.Test.ID);
-
-            endpointConfiguration.EnableInstallers();
-
-            endpointConfiguration.UseTransport(new LearningTransport { StorageDirectory = storageDir });
-
-            endpointConfiguration.RegisterComponentsAndInheritanceHierarchy(runDescriptor);
-
-            await configurationBuilderCustomization(endpointConfiguration).ConfigureAwait(false);
-
-            return endpointConfiguration;
-        }
     }
 }
---
+++ src/NServiceBus.Gateway.AcceptanceTests/TestSuitConstraints.cs
@@ -0,0 +1,21 @@
+﻿namespace NServiceBus.Gateway.AcceptanceTests
+{
+    using System.Runtime.CompilerServices;
+    using NServiceBus.AcceptanceTesting;
+    using NServiceBus.AcceptanceTesting.Support;
+
+    public partial class TestSuiteConstraints : ITestSuiteConstraints
+    {
+        public bool SupportsDtc => false;
+        public bool SupportsCrossQueueTransactions => false;
+        public bool SupportsNativePubSub => true;
+        public bool SupportsDelayedDelivery => false;
+        public bool SupportsOutbox => false;
+        public bool SupportsPurgeOnStartup => false;
+        public IConfigureEndpointTestExecution CreateTransportConfiguration() => new ConfigureEndpointAcceptanceTestingTransport(SupportsNativePubSub, SupportsDelayedDelivery);
+        public IConfigureEndpointTestExecution CreatePersistenceConfiguration() => new ConfigureEndpointAcceptanceTestingPersistence();
+
+        [ModuleInitializer]
+        public static void Initialize() => ITestSuiteConstraints.Current = new TestSuiteConstraints();
+    }
+}
