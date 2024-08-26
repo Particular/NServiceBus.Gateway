a46b8482072c987144c150ff09610fcbdd9fa776
Christian <christian.quiros+github@gmail.com>
2024-08-26 17:51:53 -0600

    Autoformat of test projects for NUnit2049

--- src/NServiceBus.Gateway.Tests/SendOptionsExtensionsTests.cs
+++ src/NServiceBus.Gateway.Tests/SendOptionsExtensionsTests.cs
@@ -29,7 +29,7 @@
             sendOptions.RouteToSites(expectedSites);
             var sites = sendOptions.GetSitesRoutingTo();
 
-            CollectionAssert.AreEqual(expectedSites, sites);
+            Assert.That(sites, Is.EqualTo(expectedSites).AsCollection);
         }
     }
 }
